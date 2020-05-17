c5071622896507a27aa0f78fd496b8de829de918
Paulo dos Santos <paulo@fazendadosoftware.com>
2020-05-17 13:10:43 -0300

    purged unecessary files

--- src/fact-sheet-mapper.js
+++
@@ -1,8 +0,0 @@
-import escape from 'lodash/escape';
-
-export default function factSheetMapper(fs) {
-  return `
-    <h6>${fs.displayName}</h6>
-    <div style="font-size: 11px">${escape(fs.description)}</div>
-  `;
-}
